823aecc689a94066be6a2a6ae1dd84de8275fdbe
itwill <itwill@DESKTOP-7T51VDU>
2024-04-22 16:38:24 +0900

    240422

---
+++ java_1/src/basic/HelloWorldApp.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class HelloWorldApp {
+	public static void main(String[] args) {
+ 		System.out.print("Hello,");
+		System.out.println("World!!!");
+		System.out.println("Good Bye.");
+	}	
+}
