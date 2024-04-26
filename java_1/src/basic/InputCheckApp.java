86ae1647149c7d2fba88df9dd38c8930296c2753
itwill <itwill@DESKTOP-7T51VDU>
2024-04-26 14:53:29 +0900

    240426

--- java_1/src/basic/InputCheckApp.java
+++ java_1/src/basic/InputCheckApp.java
@@ -14,6 +14,7 @@ public class InputCheckApp {
 		int score=scanner.nextInt();
 		*/
 		
+		/*
 		//변수에 비정상적인 값을 저장해야만 반복문 실행
 		int score=-1;
 		//비정상적인 점수가 입력된 경우 반복문을 실행하고 정상적인 점수가 입력된 경우 반복문 종료 
@@ -23,6 +24,30 @@ public class InputCheckApp {
 			if(score < 0 || score > 100) {
 				System.out.println("[에러]점수는 0~100 범위의 정수값만 입력 가능합니다.");
 			}
+		}
+		*/
+
+		/*
+		int score;
+		//비정상적인 점수가 입력된 경우 반복문을 실행하고 정상적인 점수가 입력된 경우 반복문 종료 
+		do {
+			System.out.print("점수 입력[0~100] >> ");
+			score=scanner.nextInt();
+			if(score < 0 || score > 100) {
+				System.out.println("[에러]점수는 0~100 범위의 정수값만 입력 가능합니다.");
+			}
+		} while(score < 0 || score > 100);
+		*/
+		
+		int score;
+		//조건식 대신 [true]를 사용한 경우 무한루프 발생
+		while(true) {
+			System.out.print("점수 입력[0~100] >> ");
+			score=scanner.nextInt();
+			//키보드로 입력받은 정수값이 정상적인 경우 break 명령을 사용하여 반복문 종료 
+			if(score >= 0 && score <=100) break;
+			//비정상적인 정수값이 입력된 경우 에러 메소드 출력 - 반복문 재실행
+			System.out.println("[에러]점수는 0~100 범위의 정수값만 입력 가능합니다.");
 		}
 		
 		String grade="";
