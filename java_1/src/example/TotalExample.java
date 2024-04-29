4aa5d14965b69c0c719050265a82c853eb0ce109
itwill <itwill@DESKTOP-7T51VDU>
2024-04-29 12:02:07 +0900

    240429

--- java_1/src/example/TotalExample.java
+++ java_1/src/example/TotalExample.java
@@ -1,5 +1,7 @@
 package example;
 
+import java.util.Scanner;
+
 //키보드로 정수값을 계속 입력받아 합계를 계산하여 출력하는 프로그램을 작성하세요.
 //단, 입력된 정수값이 0인 경우 입력을 종료하고 합계 출력
 //ex) 정수값 입력[0:종료] >> 10
@@ -9,18 +11,21 @@ package example;
 //    [결과]합계 = 60
 public class TotalExample {
 	public static void main(String[] args) {
-
+		Scanner scanner=new Scanner(System.in);
+		
+		int total=0;
+		
+		//정수값을 입력받아 정수값을 누적하기 위한 반복문
+		// => 정수값 [0]이 입력되면 반복문 종료
+		while(true) {
+			System.out.print("정수값 입력[0:종료] >> ");
+			int number=scanner.nextInt();
+			if(number == 0) break;
+			total+=number;
+		}
+		
+		System.out.println("[결과]합계 = "+total);
+		
+		scanner.close();
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
