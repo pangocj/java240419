7908af248f6916eddc49122bafc27baba6f1784c
itwill <itwill@DESKTOP-7T51VDU>
2024-04-30 17:56:27 +0900

    240430

--- java_2/src/oop/CarApp.java
+++ java_2/src/oop/CarApp.java
@@ -22,12 +22,19 @@ public class CarApp {
 		//문제점)객체를 사용하여 필드에 직접적인 접근을 허용하면 필드에 비정상적인 값 저장 가능
 		//해결법)클래스를 만들때 필드가 숨겨지도록 은닉화 처리하여 작성
 		// => 객체로 은닉화 처리된 필드를 사용할 경우 에러 발생
-		carOne.modelName="싼타페";//객체의 필드값 변경
+		//carOne.modelName="싼타페";//객체의 필드값 변경
+		//객체로 Setter 메소드를 호출하여 매개변수에 전달된 값으로 필드값 변경
+		carOne.setModelName("싼타페");
 		
-		System.out.println("첫번째 자동차의 모델명 = "+carOne.modelName);//객체의 필드값 출력
-		System.out.println("첫번째 자동차의 엔진상태 = "+carOne.engineStatus);
-		System.out.println("첫번째 자동차의 현재속도 = "+carOne.currentSpeed);
+		//System.out.println("첫번째 자동차의 모델명 = "+carOne.modelName);//객체의 필드값 출력
+		//System.out.println("첫번째 자동차의 엔진상태 = "+carOne.engineStatus);
+		//System.out.println("첫번째 자동차의 현재속도 = "+carOne.currentSpeed);
+		//객체로 Getter 메소드를 호출하여 필드값을 반환받아 출력 처리
+		System.out.println("첫번째 자동차의 모델명 = "+carOne.getModelName());//객체의 필드값 출력
+		System.out.println("첫번째 자동차의 엔진상태 = "+carOne.isEngineStatus());
+		System.out.println("첫번째 자동차의 현재속도 = "+carOne.getCurrentSpeed());
 		System.out.println("==============================================================");
+		/*
 		carTwo.modelName="쏘나타";
 		carTwo.engineStatus=true;
 		carTwo.currentSpeed=80;
@@ -35,6 +42,14 @@ public class CarApp {
 		System.out.println("두번째 자동차의 모델명 = "+carTwo.modelName);
 		System.out.println("두번째 자동차의 엔진상태 = "+carTwo.engineStatus);
 		System.out.println("두번째 자동차의 현재속도 = "+carTwo.currentSpeed);
+		*/
+		carTwo.setModelName("쏘나타");
+		carTwo.setEngineStatus(true);
+		carTwo.setCurrentSpeed(80);
+		
+		System.out.println("두번째 자동차의 모델명 = "+carTwo.getModelName());
+		System.out.println("두번째 자동차의 엔진상태 = "+carTwo.isEngineStatus());
+		System.out.println("두번째 자동차의 현재속도 = "+carTwo.getCurrentSpeed());
 		System.out.println("==============================================================");
 		//참조변수.메소드명(값, 값, ...) : 참조변수에 저장된 메모리 주소로 객체를 참조하여 
 		//클래스의 메소드 호출 - 기능 구현
