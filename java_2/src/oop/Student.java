83114d6eeb075b70f64bf4473be34e31ec81114c
itwill <itwill@DESKTOP-7T51VDU>
2024-05-01 16:46:37 +0900

    240501

--- java_2/src/oop/Student.java
+++ java_2/src/oop/Student.java
@@ -1,10 +1,26 @@
 package oop;
 
+//static : 객체가 아닌 클래스로 필드 또는 메소드에 접근하여 사용하기 위한 기능을 제공하는 키워드(제한자) 
+// => 클래스(내부클래스), 필드, 메소드 작성시 사용하는 제한자
+
+//제한자(Modifier) : 특별한 기능(제한)을 제공하는 키워드
+// => Access Modifier(private, package, protected, public), static, final, abstract
+
 //학생정보(학번, 이름, 국어, 영어, 총점)를 저장하기 위한 클래스
 public class Student {
+	//인스턴스 필드(Instance Field) : 객체 생성시 객체에 만들어지는 필드로 객체를 사용하여 필드에 접근
+	// => 생성자를 사용하여 객체 생성시 필드에 초기값 저장 
 	private int num;
 	private String name;
 	private int kor, eng, tot;
+	
+	//정적 필드(Static Field) : 클래스 파일(XXX.class)를 읽어 메모리(MethodArea)에 저장될 때 
+	//만들지는 필드로 클래스를 사용하여 필드에 접근
+	// => 객체 생성전에 클래스에 하나만 만들어지는 필드
+	// => 정적 필드는 직접 값을 저장하여 초기화 처리 - 생성자를 사용하여 초기화 미처리
+	// => 클래스로 생성된 모든 객체는 정적 필드 사용 가능 - 모든 객체가 값을 공유하여 사용
+	//public static int total=0;
+	private static int total=0;
 	
 	public Student() {
 		// TODO Auto-generated constructor stub
@@ -24,6 +40,10 @@ public class Student {
 		calcTot();
 	}
 
+	//인스턴스 메소드(Instance Method) : this 키워드를 제공받아 사용할 수 있는 메소드로 
+	//객체를 사용하여 호출
+	// => this 키워드를 사용하여 인스턴스 필드 및 인스턴스 메소드 사용 가능
+	// => 정적 필드 및 정적 메소드 사용 가능
 	public int getNum() {
 		return num;
 	}
@@ -72,6 +92,17 @@ public class Student {
 	public void display() {
 		System.out.print("["+name+"]님의 성적 >> ");
 		System.out.println("국어 = "+kor+", 영어 = "+eng+", 총점 = "+tot);
+	}
+
+	//정적 메소드(Static Method) : this 키워드를 제공받지 않는 메소드로 클래스를 사용하여 호출
+	// => this 키워드를 사용할 수 없으므로 인스턴스 필드 및 인스턴스 메소드 사용 불가능
+	// => 정적 필드 및 정적 메소드만 사용 가능
+	public static int getTotal() {
+		return total;
+	}
+
+	public static void setTotal(int total) {
+		Student.total = total;
 	}
 }
 
--- java_2/src/oop/StudentApp.java
+++ java_2/src/oop/StudentApp.java
@@ -44,6 +44,7 @@ public class StudentApp {
 		}
 		System.out.println("==============================================================");
 		*/
+		//int total=0;
 		
 		Student[] students={new Student(1000, "홍길동", 90, 90)
 				, new Student(2000, "임꺽정", 94, 98), new Student(3000, "전우치", 91, 80)
@@ -52,17 +53,15 @@ public class StudentApp {
 		//향상된 for 구문을 사용하여 배열 요소가 참조하는 객체를 차례대로 제공받아 일괄 처리 가능
 		for(Student student : students) {
 			student.display();
+			//total+=student.getTot();
+			//Student.total+=student.getTot();
+			Student.setTotal(Student.getTotal()+student.getTot());
 		}
+		System.out.println("==============================================================");
+		//모든 학생들의 성적 합계를 계산하여 출력
+		//System.out.println("총합계 = "+total);
+		//System.out.println("총합계 = "+Student.total);
+		System.out.println("총합계 = "+Student.getTotal());
 		System.out.println("==============================================================");
 	}
 }
-
-
-
-
-
-
-
-
-
-
