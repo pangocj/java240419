ab0fb70fbeb3ad24b3a5d3ad1abb8fcc9a82763e
itwill <itwill@DESKTOP-7T51VDU>
2024-05-01 15:11:04 +0900

    240501

--- java_2/src/oop/Member.java
+++ java_2/src/oop/Member.java
@@ -1,11 +1,27 @@
 package oop;
 
+//this : 메소드에서 메소드를 호출한 객체의 메모리 주소를 저장하기 위한 키워드
+// => 메소드에서 this 키워드로 객체를 참조하여 객체의 필드 또는 메소드 사용
+// => this 키워드를 사용하지 않아도 객체의 필드 또는 메소드 사용 가능하도록 자동 제공
+
+//메소드에 this 키워드가 존재하는 이유
+// => 클래스에 작성된 필드는 객체마다 메모리(HeapArea)에 독립적으로 생성되지만 메소드는 객체와
+//상관없이 메모리(MethodArea)에 하나만 생성되어 사용 - 프로토타입 클래스(Prototype Class)
+// => 메소드에서 필드를 사용할 경우 객체를 명확히 구분하여 객체 필드를 사용하기 위한 this 키워드 제공
+
+//this 키워드를 사용하는 경우
+//1.메소드의 매개변수의 이름을 필드의 이름과 동일하게 작성한 경우 필드를 표현하기 위해 this 키워드 사용
+// => 생성자 또는 Setter 메소드를 작성할 경우 사용
+//2.생성자에서 다른 생성자를 호출하여 필드 초기화 처리하기 위해 this 키워드 사용
+//3.이벤트 처리 프로그램 또는 다중 스레드 프로그램에서 객체를 표현하기 위해 this 키워드 사용
+
 //회원정보(아이디, 이름, 이메일)를 저장하기 위한 클래스
 public class Member {
 	//필드(Field) : 표현대상의 속성값을 저장하기 위한 클래스의 요소
-	private String id;
-	private String name;
-	private String email;
+	// => 필드 작성시 필드에 저장된 초기값 설정 가능 : 객체 생성시 기본값이 아닌 초기값 저장
+	private String id="NoId";
+	private String name="NoName";
+	private String email="NoEmail";
 
 	//생성자(Constructor) : 객체를 생성하기 위한 특별한 메소드
 	// => 생성자를 작성하지 않으면 매개변수가 없는 기본 생성자를 자동으로 생성하여 제공
@@ -22,7 +38,11 @@ public class Member {
 	//이클립스 기능을 사용하여 기본 생성자 자동 작성 가능
 	// => [Ctrl]+[Space] >> 나열된 목록 중 Constructor 선택
 	public Member() {
-		// TODO Auto-generated constructor stub
+		//this 키워드를 사용하여 매개변수가 있는 생성자를 호출하여 필드 초기화 처리
+		//형식) this(값, 값, ...)
+		// => 생성자에 this 키워드로 다른 생성자를 호출하는 명령
+		// => this 키워드로 생성자를 호출하는 명령은 무조건 생성자의 첫번째 명령으로 작성
+		//this("NoId", "NoName", "NoEmail");
 	}
 	
 	//매개변수가 있는 생성자 작성
