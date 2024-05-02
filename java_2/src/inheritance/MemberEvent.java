589ba0ddef7824ec004661768ad26d9625cbe814
itwill <itwill@DESKTOP-7T51VDU>
2024-05-02 17:14:04 +0900

    240502

--- java_2/src/inheritance/MemberEvent.java
+++ java_2/src/inheritance/MemberEvent.java
@@ -26,7 +26,8 @@ package inheritance;
 //형식) super(값, 값, ...);
 // => 자식클래스의 생성자에서 부모클래스의 매개변수가 있는 생성자를 호출하는 명령 - 부모클래스의 객체 필드 초기화
 // => 자식클래스의 생성자에서 부모클래스의 생성자를 호출하는 명령은 반드시 첫번째 명령으로 작성
-//2.
+//2.자식클래스에서 부모클래스의 메소드를 오버라이딩 선언한 경우 자식클래스의 메소드에서
+//부모클래스의 숨겨진 메소드를 호출하기 위해 super 키워드 사용
 
 //이벤트 관련 회원정보(아이디, 이름, 이메일)를 저장하기 위한 클래스
 // => 회원정보를 저장하기 위한 클래스(Member 클래스)를 상속박아 작성하는 것을 권장
@@ -90,11 +91,33 @@ public class MemberEvent extends Member {
 		this.email = email;
 	}
 	
+	//메소드 오버라이딩(Method Overriding) : 상속관계의 클래스에서 부모클래스의 메소드를 
+	//자식클래스에서 다시 선언하는 기능
+	// => 부모클래스의 메소드를 자식클래스에서 사용하기 불적절한 경우 메소드 오버라이딩 사용 
+	// => 부모클래스의 메소드와 같은 접근 제한자, 반환형, 메소드명, 매개변수, 예외전달을 
+	//자식클래스에서 동일하게 작성 - 메소드 오버라이딩 작성 규칙
+	// => 메소드 오버라이딩 선언하면 부모클래스의 메소드(Hide Method)는 숨겨지고 자식클래스의
+	//메소드만 사용되도록 실행
+	/*
 	public void display() {
 		//System.out.println("아이디 = "+id);
 		System.out.println("아이디 = "+getId());
 		//System.out.println("이름 = "+name);
 		System.out.println("이름 = "+getName());
+		System.out.println("이메일 = "+email);
+	}
+	*/
+	
+	//이클립스에서는 부모클래스의 메소드를 자식클래스에서 오버라이딩 선언되도록 자동 완성하는 기능 제공
+	// => 오버라이딩 선언하고 싶은 부모클래스의 메소드명 입력 >> [Ctrl]+[Space] >> Override 메소드 선택
+	//@Override : 오버라이딩 선언된 메소드를 표현하기 위한 어노테이션
+	// => 메소드 작성시 오버라이딩 작성 규칙을 위반한 경우 에러 발생
+	//어노테이션(Annotation) : 특별한 기능(명령의 모임)을 제공하기 위한 Java 자료형
+	// => @Override, @Deprecated, @SuppressWarnings
+	@Override
+	public void display() {
+		//super 키워드를 사용해 부모클래스의 숨겨진 메소드 호출 
+		super.display();
 		System.out.println("이메일 = "+email);
 	}
 }
