076eb63cb5ee3fb5ae523bb397f1fde939f81d00
itwill <itwill@DESKTOP-7T51VDU>
2024-05-07 12:04:40 +0900

    240507

--- java_2/src/xyz/itwill/app/SubjectApp.java
+++ java_2/src/xyz/itwill/app/SubjectApp.java
@@ -1,5 +1,9 @@
 package xyz.itwill.app;
 
+import xyz.itwill.subject.JavaSubject;
+import xyz.itwill.subject.OracleSubject;
+import xyz.uniwill.subject.NetworkSubject;
+
 //패키지(Package) : 같은 목적의 Java 자료형(참조형)을 명확히 구분하여 저장하기 위한 폴더(디렉토리)
 // => Java 자료형 : 클래스(Class), 인터페이스(Interface), 열거법(Enum), 레코드(Record), 어노테이션(Annotation)
 // => Java 자료형을 쉽게 구분하여 사용하기 위해 패키지를 사용하는 것을 권장
@@ -14,18 +18,37 @@ package xyz.itwill.app;
 //형식) package 패키지경로;
 // => Java 자료형이 특정 패키지에 존재하는지를 명확히 구분하여 제공
 
+//import : 다른 패키지에 작성된 Java 자료형을 명확히 표현하여 제공하기 위한 키워드
+//형식) import 패키지경로.자료형;
+// => Java 자료형 대신 [*] 기호 사용하여 패키지의 작성된 모든 Java 자료형 제공 가능
+// => package 선언문 뒤에 작성하고 Java 자료형 선언 전에 작성
+//이클립스에서는 프로그램 작성에 필요한 Java 자료형을 import 선언문으로 처리하거나 불필요한
+//Java 자료형의 import 선언문을 제거하는 기능 제공 
+// => [Ctrl]+[Shift]+[O] - import 선언문을 정리하는 단축키
+
 public class SubjectApp {
+	public static void main(String[] args) {
+		//같은 패키지에 선언된 Java 자료형은 패키지 경로를 표현하지 않아도 Java 자료형 사용 가능
+		//다른 패키지에 선언된 Java 자료형은 패키지 경로를 반드시 표현해야만 Java 자료형 사용 가능
+		//xyz.itwill.subject.OracleSubject subject1=new xyz.itwill.subject.OracleSubject();
+		
+		//다른 패키지에 선언된 Java 자료형을 import 처리하면 패키지 경로를 표현하지 않고 
+		//Java 자료형 사용 가능
+		OracleSubject subject1=new OracleSubject();
+		subject1.display();
 
+		//이클립스에서는 다른 패키지의 Java 자료형을 사용할 경우 import 선언문을 자동으로 생성하는 기능 제공
+		// => 자료형 이름 >> [Ctrl]+[Space] >> 엔터(Enter)
+		NetworkSubject subject2=new NetworkSubject();
+		subject2.display();
+		
+		//같은 이름의 Java 자료형이 여러개인 경우 패키지를 잘 구분하여 자료형 선택하여 사용
+		JavaSubject subject3=new JavaSubject();
+		subject3.display();
+		
+		//같은 이름의 Java 자료형이 이미 import 처리되어 있는 경우 import 처리 불가능 
+		// => 패키지 경로를 표현하여 Java 자료형 사용 
+		xyz.uniwill.subject.JavaSubject subject4=new xyz.uniwill.subject.JavaSubject();
+		subject4.display();
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
