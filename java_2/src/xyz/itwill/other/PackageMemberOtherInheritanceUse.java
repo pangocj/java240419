660a399b46edbf13749a089d0bcefe7cf4f2020b
itwill <itwill@DESKTOP-7T51VDU>
2024-05-07 14:58:07 +0900

    240507

---
+++ java_2/src/xyz/itwill/other/PackageMemberOtherInheritanceUse.java
@@ -0,0 +1,13 @@
+package xyz.itwill.other;
+
+import xyz.itwill.access.PackageMember;
+
+public class PackageMemberOtherInheritanceUse extends PackageMember {
+	public void run() {
+		//부모클래스(PackageMember)의 필드 또는 메소드 사용
+		/*
+		num=100;
+		display();
+		*/
+	}
+}
