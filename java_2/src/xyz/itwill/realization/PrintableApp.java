cc833fd04218d57d0ab07437e3379b6871c9a9b1
itwill <itwill@DESKTOP-7T51VDU>
2024-05-08 11:34:18 +0900

    240508

---
+++ java_2/src/xyz/itwill/realization/PrintableApp.java
@@ -0,0 +1,18 @@
+package xyz.itwill.realization;
+
+public class PrintableApp {
+	public static void main(String[] args) {
+		Printable printOne=new PrintSingle();
+			
+		Printable.power();//인터페이스를 사용하여 정적메소드 호출
+		printOne.print();//묵시적 객체 형변환에 의해 자식클래스의 오버라이딩 선언된 메소드 호출
+		printOne.scan();//부모인터페이스의 기본메소드 호출
+		System.out.println("==============================================================");
+		Printable printTwo=new PrintMulti();
+		
+		Printable.power();
+		printTwo.print();
+		printTwo.scan();//묵시적 객체 형변환에 의해 자식클래스의 오버라이딩 선언된 메소드 호출
+		System.out.println("==============================================================");
+	}
+}
