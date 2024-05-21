e896dd906e957be56fea18efc7042514ae0d872d
itwill <itwill@DESKTOP-7T51VDU>
2024-05-21 14:55:28 +0900

    240521

--- java_3/src/xyz/itwill/swing/StudentFrameApp.java
+++ java_3/src/xyz/itwill/swing/StudentFrameApp.java
@@ -21,9 +21,6 @@ public class StudentFrameApp extends JFrame {
 	JTable table;
 	private StudentDialog studentDialog;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
