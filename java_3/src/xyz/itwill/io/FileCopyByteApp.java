e2417cc362ad5c67e4095d37b2f84d93e9f9b480
itwill <itwill@DESKTOP-7T51VDU>
2024-05-22 12:47:24 +0900

    240522

---
+++ java_3/src/xyz/itwill/io/FileCopyByteApp.java
@@ -0,0 +1,9 @@
+package xyz.itwill.io;
+
+//원본파일(c:\data\bandizip.exe)에 저장된 내용을 원시데이타로 읽어 타겟파일(c:\data\bandizip_byte.exe)에
+//전달하여 저장하는 프로그램 작성 - 파일 복사 프로그램 
+public class FileCopyByteApp {
+	public static void main(String[] args) {
+		
+	}
+}
