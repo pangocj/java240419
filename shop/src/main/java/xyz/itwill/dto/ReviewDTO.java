c89735e0bd3af39130da1e87bed760e45a873c67
itwill <itwill@DESKTOP-7T51VDU>
2024-07-15 15:16:56 +0900

    240715

--- shop/src/main/java/xyz/itwill/dto/ReviewDTO.java
+++ shop/src/main/java/xyz/itwill/dto/ReviewDTO.java
@@ -15,18 +15,139 @@ create sequence review_seq;
 -------------------- -------- -------------- 
 REVIEW_NUM           NOT NULL NUMBER         - 글번호         
 REVIEW_MEMBER_NUM             NUMBER         - 작성자 : 로그인 사용자의 회원번호
-REVIEW_SUBJECT                VARCHAR2(500)  
-REVIEW_CONTENT                VARCHAR2(4000) 
-REVIEW_IMAGE                  VARCHAR2(100)  
-REVIEW_REGISTER_DATE          DATE           
-REVIEW_UPDATE_DATE            DATE           
-REVIEW_COUNT                  NUMBER         
-REVIEW_REF                    NUMBER         
-REVIEW_RESTEP                 NUMBER         
-REVIEW_RELEVEL                NUMBER         
-REVIEW_STATUS                 NUMBER(1)   
+REVIEW_SUBJECT                VARCHAR2(500)  - 제목
+REVIEW_CONTENT                VARCHAR2(4000) - 내용 
+REVIEW_IMAGE                  VARCHAR2(100)  - 이미지 파일의 이름 
+REVIEW_REGISTER_DATE          DATE           - 작성날짜
+REVIEW_UPDATE_DATE            DATE           - 변경날짜
+REVIEW_IP			          VARCHAR2(20)   - 클라이언트 IP 주소    
+REVIEW_COUNT                  NUMBER         - 조횟수
+REVIEW_REF                    NUMBER         - 답글 : 글그룹
+REVIEW_RESTEP                 NUMBER         - 답글 : 글순서
+REVIEW_RELEVEL                NUMBER         - 답글 : 글깊이
+REVIEW_STATUS                 NUMBER(1)      - 글상태 : 0(삭제글), 1(일반글), 2(비밀글)
 */
 
 public class ReviewDTO {
+	private int reviewNum;
+	private int reviewMemberNum;
+	private String reviewSubject;
+	private String reviewContent;
+	private String reviewImage;
+	private String reviewRegisterDate;
+	private String reviewUpdateDate;
+	private String reviewIp;
+	private int reviewCount;
+	private int reviewRef;
+	private int reviewRestep;
+	private int reviewRelevel;
+	private int reviewStatus;
 
+	public ReviewDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getReviewNum() {
+		return reviewNum;
+	}
+
+	public void setReviewNum(int reviewNum) {
+		this.reviewNum = reviewNum;
+	}
+
+	public int getReviewMemberNum() {
+		return reviewMemberNum;
+	}
+
+	public void setReviewMemberNum(int reviewMemberNum) {
+		this.reviewMemberNum = reviewMemberNum;
+	}
+
+	public String getReviewSubject() {
+		return reviewSubject;
+	}
+
+	public void setReviewSubject(String reviewSubject) {
+		this.reviewSubject = reviewSubject;
+	}
+
+	public String getReviewContent() {
+		return reviewContent;
+	}
+
+	public void setReviewContent(String reviewContent) {
+		this.reviewContent = reviewContent;
+	}
+
+	public String getReviewImage() {
+		return reviewImage;
+	}
+
+	public void setReviewImage(String reviewImage) {
+		this.reviewImage = reviewImage;
+	}
+
+	public String getReviewRegisterDate() {
+		return reviewRegisterDate;
+	}
+
+	public void setReviewRegisterDate(String reviewRegisterDate) {
+		this.reviewRegisterDate = reviewRegisterDate;
+	}
+
+	public String getReviewUpdateDate() {
+		return reviewUpdateDate;
+	}
+
+	public void setReviewUpdateDate(String reviewUpdateDate) {
+		this.reviewUpdateDate = reviewUpdateDate;
+	}
+
+	public String getReviewIp() {
+		return reviewIp;
+	}
+
+	public void setReviewIp(String reviewIp) {
+		this.reviewIp = reviewIp;
+	}
+
+	public int getReviewCount() {
+		return reviewCount;
+	}
+
+	public void setReviewCount(int reviewCount) {
+		this.reviewCount = reviewCount;
+	}
+
+	public int getReviewRef() {
+		return reviewRef;
+	}
+
+	public void setReviewRef(int reviewRef) {
+		this.reviewRef = reviewRef;
+	}
+
+	public int getReviewRestep() {
+		return reviewRestep;
+	}
+
+	public void setReviewRestep(int reviewRestep) {
+		this.reviewRestep = reviewRestep;
+	}
+
+	public int getReviewRelevel() {
+		return reviewRelevel;
+	}
+
+	public void setReviewRelevel(int reviewRelevel) {
+		this.reviewRelevel = reviewRelevel;
+	}
+
+	public int getReviewStatus() {
+		return reviewStatus;
+	}
+
+	public void setReviewStatus(int reviewStatus) {
+		this.reviewStatus = reviewStatus;
+	}
 }
