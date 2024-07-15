52a66f979c6620cd2946791d43c4303603eb10cc
itwill <itwill@DESKTOP-7T51VDU>
2024-07-15 15:29:30 +0900

    240715

---
+++ shop/src/main/java/xyz/itwill/dao/ReviewDAO.java
@@ -0,0 +1,19 @@
+package xyz.itwill.dao;
+
+public class ReviewDAO extends JdbcDAO {
+	private static ReviewDAO _dao;
+	
+	private ReviewDAO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	static {
+		_dao=new ReviewDAO();
+	}
+	
+	public static ReviewDAO getDAO() {
+		return _dao;
+	}
+	
+	
+}
