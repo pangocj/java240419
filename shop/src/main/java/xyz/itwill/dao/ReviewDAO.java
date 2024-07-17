aad8656b8f307be239ecce8bea03579c5b101e1e
itwill <itwill@DESKTOP-7T51VDU>
2024-07-17 17:07:42 +0900

    240717

--- shop/src/main/java/xyz/itwill/dao/ReviewDAO.java
+++ shop/src/main/java/xyz/itwill/dao/ReviewDAO.java
@@ -246,6 +246,30 @@ public class ReviewDAO extends JdbcDAO {
 		}
 		return review;
 	}
+	
+	//글번호를 전달받아 REVIEW 테이블에 저장된 행의 게시글조횟수를 1 증가되도록 변경하고
+	//변경행의 갯수를 반환하는 메소드
+	public int updateReviewCount(int ref, int restep) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		int rows=0;
+		try {
+			con=getConnection();
+			
+			String sql="update review set review_restep=review_restep+1"
+					+ " where review_ref=? and review_restep>?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1, ref);
+			pstmt.setInt(2, restep);
+			
+			rows=pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("[에러]updateReviewRestep() 메소드의 SQL 오류 = "+e.getMessage());
+		} finally {
+			close(con, pstmt);
+		}
+		return rows;
+	}
 }
 
 
