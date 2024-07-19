256575ee99d751cf9148b095d002cbb8d3628ca2
itwill <itwill@DESKTOP-7T51VDU>
2024-07-19 16:59:48 +0900

    240719

---
+++ ajax/src/main/java/xyz/itwill/dto/AjaxMemberDTO.java
@@ -0,0 +1,8 @@
+package xyz.itwill.dto;
+
+//create table ajax_member(id varchar2(30) primary key, passwd varchar2(100)
+//	    , name varchar2(50), email varchar2(100));
+
+public class AjaxMemberDTO {
+
+}
