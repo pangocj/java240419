d957afbe873c604298f6c4a73d87de186a6458a9
itwill <itwill@DESKTOP-7T51VDU>
2024-07-19 17:00:20 +0900

    240719

--- ajax/src/main/java/xyz/itwill/dto/AjaxMemberDTO.java
+++ ajax/src/main/java/xyz/itwill/dto/AjaxMemberDTO.java
@@ -4,5 +4,44 @@ package xyz.itwill.dto;
 //	    , name varchar2(50), email varchar2(100));
 
 public class AjaxMemberDTO {
+	private String id;
+	private String passwd;
+	private String name;
+	private String email;
+	
+	public AjaxMemberDTO() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
