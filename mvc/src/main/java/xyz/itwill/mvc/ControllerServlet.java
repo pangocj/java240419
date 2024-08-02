3df7fb3128b4b57d37f28b1732550fba26cd951f
itwill <itwill@DESKTOP-7T51VDU>
2024-08-02 11:26:55 +0900

    240801

--- mvc/src/main/java/xyz/itwill/mvc/ControllerServlet.java
+++ mvc/src/main/java/xyz/itwill/mvc/ControllerServlet.java
@@ -70,7 +70,9 @@ public class ControllerServlet extends HttpServlet {
 			action=new LoginModel();
 		} else if(command.equals("/logout.do")) {
 			action=new LogoutModel();
-		} else if(command.equals("/error.do")) {
+		} else if(command.equals("/writeform.do")) {
+			action=new WriteFormModel();
+		}  else if(command.equals("/error.do")) {
 			action=new ErrorModel();
 		} else {
 			action=new ErrorModel();
