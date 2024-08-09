af797920850bc3dfa794b68e82cfe441ca819d2a
itwill <itwill@DESKTOP-7T51VDU>
2024-08-09 13:03:35 +0900

    240809

--- mybatis/src/main/java/xyz/itwill/dao/MyUserDAO.java
+++ mybatis/src/main/java/xyz/itwill/dao/MyUserDAO.java
@@ -1,6 +1,13 @@
 package xyz.itwill.dao;
 
-public class MyUserDAO {
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import xyz.itwill.dto.MyUser;
+import xyz.itwill.mapper.MyUserMapper;
+
+public class MyUserDAO extends AbstractSession {
 	private static MyUserDAO _dao;
 	
 	private MyUserDAO() {
@@ -15,5 +22,31 @@ public class MyUserDAO {
 		return _dao;
 	}
 	
+	public int insertUser(MyUser user) {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyUserMapper.class).insertUser(user);
+		} finally {
+			sqlSession.close();
+		}
+	}
 	
+	public List<MyUser> selectUserList() {
+		SqlSession sqlSession=getSqlSessionFactory().openSession(true);
+		try {
+			return sqlSession.getMapper(MyUserMapper.class).selectUserList();
+		} finally {
+			sqlSession.close();
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
