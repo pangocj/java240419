e7c6a6e6fd47d7823d4f125c9a7bf6d894643a5e
itwill <itwill@DESKTOP-7T51VDU>
2024-08-12 15:11:49 +0900

    240812

--- mybatis/src/main/java/xyz/itwill/mapper/MyReplyMapper.java
+++ mybatis/src/main/java/xyz/itwill/mapper/MyReplyMapper.java
@@ -3,8 +3,10 @@ package xyz.itwill.mapper;
 import java.util.List;
 
 import xyz.itwill.dto.MyReply;
+import xyz.itwill.dto.MyReplyUser;
 
 public interface MyReplyMapper {
 	int insertReply(MyReply reply);
 	List<MyReply> selectReplyList();
+	List<MyReplyUser> selectReplyUserList1();
 }
