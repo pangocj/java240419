47e442df3e0c8ad61bd5d09cad7d2ca594e84c6e
itwill <itwill@DESKTOP-7T51VDU>
2024-08-21 17:03:43 +0900

    240821

---
+++ spring/src/main/java/xyz/itwill04/bean/ComponentAnnotationBean.java
@@ -0,0 +1,11 @@
+package xyz.itwill04.bean;
+
+public class ComponentAnnotationBean {
+	public ComponentAnnotationBean() {
+		System.out.println("### ComponentAnnotationBeanApp 클래스의 기본 생성자 호출 ###");
+	}
+	
+	public void display() {
+		System.out.println("*** ComponentAnnotationBeanApp 클래스의 display() 메소드 호출 ***");
+	}
+}
