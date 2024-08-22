92eea4212b0da8e9e5621fa1fc4748f7456a7ac4
itwill <itwill@DESKTOP-7T51VDU>
2024-08-22 14:55:53 +0900

    240822

--- spring/src/main/java/xyz/itwill05/di/StudentApp.java
+++ spring/src/main/java/xyz/itwill05/di/StudentApp.java
@@ -28,6 +28,16 @@ public class StudentApp {
 		Student student6=context.getBean("student6", Student.class);
 		System.out.println(student6);
 		System.out.println("==========================================================");
+		//스프링 컨테이너로부터 Service 클래스의 객체를 제공받아 참조변수에 저장 
+		StudentServiceImpl service=context.getBean("studentServiceImpl", StudentServiceImpl.class);
+		
+		//참조변수에 저장된 Service 객체를 참조해 메소드를 호출하여 데이타 처리 기능 구현
+		service.addStudent(student1);
+		service.modifyStudent(student1);
+		service.removeStudent(1000);
+		service.getStudent(1000);
+		service.getStudentList();
+		System.out.println("==========================================================");
 		((ClassPathXmlApplicationContext)context).close();
 	}
 }
