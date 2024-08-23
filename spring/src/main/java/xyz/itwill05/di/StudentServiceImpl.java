ff347314f37d2308eff1b304d2140f0a2a71bd01
itwill <itwill@DESKTOP-7T51VDU>
2024-08-23 11:27:06 +0900

    240822

--- spring/src/main/java/xyz/itwill05/di/StudentServiceImpl.java
+++ spring/src/main/java/xyz/itwill05/di/StudentServiceImpl.java
@@ -3,10 +3,10 @@ package xyz.itwill05.di;
 import java.util.List;
 
 //Service 클래스 : 프로그램 실행에 필요한 데이타 처리 기능을 제공하기 위한 클래스 - 컴퍼넌트
-// => Service 클래스의 메소드는 DAO 클래스의 객체로 다수의 메소드를 호출하여 작성 - DAO 모듈화
-// => Service 클래스의 메소드에서 DAO 클래스의 메소드를 호출하기 위해 Service 클래스에 DAO
+// => Service 클래스의 메소드는 DAO 클래스의 객체로 다수의 메소드를 호출하여 기능 구현 - DAO 모듈화
+// => Service 클래스의 메소드에서 DAO 클래스의 메소드를 호출하기 위해 Service 클래스에 DAO 클래스의
 //객체를 저장할 수 있는 필드 작성 - DAO 클래스와 Service 클래스를 포함관계(의존관계)로 설정
-// => Service 클래스 교체돼도 Service 클래스를 사용하는 클래스(Medel 클래스)에 영향을 최소화 하기
+// => Service 클래스가 교체돼도 Service 클래스를 사용하는 클래스(Medel 클래스)에 영향을 최소화 하기
 //위해 반드시 인터페이스를 상속받아 작성하는 것을 권장 - 결합도를 낮춰 유지보수의 효율성 증가
 public class StudentServiceImpl implements StudentService {
 	//StudentJdbcDAOImpl 클래스를 사용하여 필드 작성
@@ -20,7 +20,7 @@ public class StudentServiceImpl implements StudentService {
 	//StudentDAO 인터페이스를 사용하여 필드 작성
 	// => StudentDAO 인터페이스를 상속받은 자식클래스의 객체를 제공받아 필드에 저장해야만 의존관계 성립
 	// => StudentDAO 인터페이스의 필드로 추상메소드를 호출하면 묵시적 객체 형변환에 의해 필드에
-	//저장된 자식클래스의 객체의 오버라이드 선언된 메소드 호출 - 오버라이드에 의한 다형성
+	//저장된 자식클래스 객체의 오버라이드 선언된 메소드 호출 - 오버라이드에 의한 다형성
 	// => DAO 클래스가 교체돼도 Service 클래스의 영향 최소화하여 객체간의 결합도를 낮춰 
 	//유지보수의 효율성 증가
 	private StudentDAO studentDAO;
