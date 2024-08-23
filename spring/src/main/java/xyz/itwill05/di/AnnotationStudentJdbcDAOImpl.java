d8ca4441b55b4b5b0c88693218d592efaca31d9e
itwill <itwill@DESKTOP-7T51VDU>
2024-08-23 12:07:17 +0900

    240823

--- spring/src/main/java/xyz/itwill05/di/AnnotationStudentJdbcDAOImpl.java
+++ spring/src/main/java/xyz/itwill05/di/AnnotationStudentJdbcDAOImpl.java
@@ -2,6 +2,12 @@ package xyz.itwill05.di;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
+//@Component : 클래스를 스프링 컨테이너가 관리할 수 있는 Spring Bean으로 등록하기 위한 어노테이션
+// => 클래스의 이름이 자동으로 식별자(beanName)으로 설정 - 클래스명의 첫문자는 소문자로 변환
+// => @Component 어노테이션의 value 속성을 사용하여 beanName 변경 가능
+@Component
 public class AnnotationStudentJdbcDAOImpl implements StudentDAO {
 	public AnnotationStudentJdbcDAOImpl() {
 		System.out.println("### AnnotationStudentJdbcDAOImpl 클래스의 기본 생성자 호출 ###");
--- spring/src/main/java/xyz/itwill05/di/AnnotationStudentServiceImpl.java
+++ spring/src/main/java/xyz/itwill05/di/AnnotationStudentServiceImpl.java
@@ -2,28 +2,33 @@ package xyz.itwill05.di;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+//@Component 어노테이션의 value 속성외 다른 속성이 없는 경우 속성값만 설정 가능
+//@Component(value = "studentService")
+@Component("studentService")
 public class AnnotationStudentServiceImpl implements StudentService {
+	//@Autowired : 스프링 컨테이너로부터 Spring Bean으로 등록된 클래스의 객체를 제공받아 필드에
+	//자동으로 저장되도록 의존성 주입을 하기 위한 어노테이션
+	
+	//클래스의 필드에 @Autowired 어노테이션을 사용하여 의존성 주입 - 필드 레벨의 의존성 주입
+	// => 필드가 여러개 작성된 경우 필드마다 @Autowired 어노테이션을 사용해 의존성 주입 
+	// => bean 엘리먼트의 autowire 속성값을 [byType]으로 설정한 경우 같은 방법으로 의존성 주입
+	// => Setter Injection 기능을 사용하지만 필드의 Setter 메소드가 없어도 의존성 주입 가능
+	
+	//문제점 : 필드의 자료형이 인터페이스인 경우 인터페이스를 상속받은 자식클래스가 Spring Bean으로
+	//2개이상 등록된 경우 의존성 주입 실패 - 예외 발생
+	//해결법-1 : 다수의 클래스 중 의존성 주입하고자 하는 클래스의 식별자(beanName)을 필드명과
+	//동일하게 변경 - 동일한 자료형의 Spring Bean이 여러개인 경우 autowire 속성값을 [byName]으로
+	//설정한 경우와 같은 방법으로 의존성 주입
+	@Autowired
 	private StudentDAO studentDAO;
 	
 	public AnnotationStudentServiceImpl() {
-		System.out.println("### StudentServiceImpl 클래스의 기본 생성자 호출 ###");
+		System.out.println("### AnnotationStudentServiceImpl 클래스의 기본 생성자 호출 ###");
 	}
 	
-	public AnnotationStudentServiceImpl(StudentDAO studentDAO) {
-		super();
-		this.studentDAO = studentDAO;
-		System.out.println("### AnnotationStudentServiceImpl 클래스의 매개변수가 작성된 생성자 호출 ###");
-	}
-
-	public StudentDAO getStudentDAO() {
-		return studentDAO;
-	}
-
-	public void setStudentDAO(StudentDAO studentDAO) {
-		this.studentDAO = studentDAO;
-		System.out.println("*** AnnotationStudentServiceImpl 클래스의 setStudentDAO(StudentDAO studentDAO) 메소드 호출 ***");
-	}
-
 	@Override
 	public void addStudent(Student student) {
 		System.out.println("*** AnnotationStudentServiceImpl 클래스의 addStudent(Student student) 메소드 호출 ***");
