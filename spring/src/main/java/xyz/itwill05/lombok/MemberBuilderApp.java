e532e825344ef5cc1d7dce8a49cb1978258b6c45
itwill <itwill@DESKTOP-7T51VDU>
2024-08-23 17:10:51 +0900

    240823

---
+++ spring/src/main/java/xyz/itwill05/lombok/MemberBuilderApp.java
@@ -0,0 +1,7 @@
+package xyz.itwill05.lombok;
+
+public class MemberBuilderApp {
+	public static void main(String[] args) {
+		
+	}
+}
--- spring/src/main/java/xyz/itwill05/lombok/MemberServiceApp.java
+++ spring/src/main/java/xyz/itwill05/lombok/MemberServiceApp.java
@@ -3,7 +3,7 @@ package xyz.itwill05.lombok;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class MemberApp {
+public class MemberServiceApp {
 	public static void main(String[] args) {
 		System.out.println("=============== Spring Container 초기화 전 ===============");
 		ApplicationContext context=new ClassPathXmlApplicationContext("05-5_lombok.xml");
