9f7d41e59e36852bbc9649394c1bce751cff23b9
itwill <itwill@DESKTOP-7T51VDU>
2024-08-27 14:56:14 +0900

    240827

---
+++ spring/src/main/java/xyz/itwill06/aop/EmailSendBean.java
@@ -0,0 +1,13 @@
+package xyz.itwill06.aop;
+
+//JavaMail 기능을 구현하기 위해 spring-context-support 라이브러리와 javax.mail 라이브러리를
+//프로젝트에 빌드되도록 처리 - 메이븐 사용 : pom.xml
+
+//메일 전송 기능(JavaMail)을 제공하기 위한 클래스 - 메일 서버를 사용해 이메일 전송
+// => 메일 서버(Mail Server) : 메일을 송수신하는 서비스를 제공하는 컴퓨터 
+// => 메일 서버는 SMTP 서비스(Simple Message Transfer Protocol - 25)를 이용해 메일을 전송하고
+//POP3(Post Office Protocol3 - 110) 서비스 또는 IMAP(Internet Message Access Protocol - 143)
+//서비스를 이용해 메일을 수신하여 사용자에게 전달
+public class EmailSendBean {
+
+}
