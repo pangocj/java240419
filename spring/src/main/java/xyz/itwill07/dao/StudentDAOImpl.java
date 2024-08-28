08a58b293065a45c509b62208d065679c570e80f
itwill <itwill@DESKTOP-7T51VDU>
2024-08-28 12:56:05 +0900

    240828

--- spring/src/main/java/xyz/itwill07/dao/StudentDAOImpl.java
+++ spring/src/main/java/xyz/itwill07/dao/StudentDAOImpl.java
@@ -2,6 +2,7 @@ package xyz.itwill07.dao;
 
 import java.util.List;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import lombok.Setter;
@@ -25,7 +26,7 @@ public class StudentDAOImpl implements StudentDAO {
 		String sql="insert into student values(?,?,?,?,?)";
 		//JdbcTemplate.update(String sql, Object ... args) : SQL 명령(INSERT, UPDATE, DELETE)을
 		//DBMS 서버에 전달하여 실행하는 메소드 - 조작행의 갯수(int) 반환
-		// => 메소드 매개변수에는 SQL 명령과 SQL 명령에 필요한 값을 차례대로 전달하여 제공
+		// => 매개변수에는 SQL 명령과 SQL 명령에 필요한 값을 차례대로 전달하여 제공
 		// => SQL 명령에 필요한 값은 InParameter(?)의 갯수와 순서에 맞게 차례대로 전달
 		return jdbcTemplate.update(sql, student.getNo(), student.getName()
 				, student.getPhone(), student.getAddress(), student.getBirthday());
@@ -45,16 +46,74 @@ public class StudentDAOImpl implements StudentDAO {
 		return jdbcTemplate.update("delete from student where no=?", no);
 	}
 
+	//학번을 전달받아 STUDENT 테이블에 저장된 행을 검색하여 학생정보를 반환하는 메소드 
 	@Override
 	public Student selectStudent(int no) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			String sql="select no, name, phone, address, birthday from student where no=?";
+			//JdbcTemplate.queryForObject(String sql, RowMapper<T> rowMapper, Object ... object)
+			// => SQL 명령(SELECT)을 DBMS 서버에 전달하여 실행하는 메소드 
+			// => 단일행의 검색결과를 DTO 객체(Wrapper 또는 String)로 반환하기 위해 사용하는 메소드
+			// => 매개변수에는 SQL 명령과 검색행을 Java 객체로 변환하는 정보를 제공하는 RowMapper 객체
+			//, SQL 명령에 필요한 값을 차례대로 전달하여 제공
+			//RowMapper 객체 : 검색행을 Java 객체로 변환하기 위한 정보를 저장한 객체
+			// => RowMapper 인터페이스의 추상메소드를 오버라이드 선언하여 메소드에 검색행의 컬럼값을
+			//객체 필드에 저장하기 위한 매핑정보를 제공하는 명령 작성
+			// => RowMapper 인터페이스의 제네릭은 검색행이 변환될 Java 객체의 자료형(클래스) 설정
+			/*
+			return jdbcTemplate.queryForObject(sql, new RowMapper<Student>() {
+				@Override
+				public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+					//검색행을 Java 객체로 변환하여 반환하는 명령 작성
+					// => 검색행의 컬럼값이 Java 객체 필드에 저장되도록 설정
+					return Student.builder()
+							.no(rs.getInt("no"))
+							.name(rs.getString("name"))
+							.phone(rs.getString("phone"))
+							.address(rs.getString("address"))
+							.birthday(rs.getString("birthday"))
+							.build();
+				}
+			}, no);
+			*/
+			
+			return jdbcTemplate.queryForObject(sql, new StudentRowMapper(), no);
+		} catch (EmptyResultDataAccessException e) {
+			//EmptyResultDataAccessException : queryForObject() 메소드로 전달되어 실행된 
+			//SELECT 명령에 대한 검색행이 없는 경우 발생되는 예외
+			return null;
+		}
 	}
 
 	@Override
 	public List<Student> selectStudentList() {
-		// TODO Auto-generated method stub
-		return null;
+		String sql="select no, name, phone, address, birthday from student order by no";
+		//JdbcTemplate.query(String sql, RowMapper<T> rowMapper, Object ... object)
+		// => SQL 명령(SELECT)을 DBMS 서버에 전달하여 실행하는 메소드 
+		// => 다수행의 검색결과를 List 객체로 반환하기 위해 사용하는 메소드
+		// => 매개변수에는 SQL 명령과 검색행을 Java 객체로 변환하는 정보를 제공하는 RowMapper 객체
+		//, SQL 명령에 필요한 값을 차례대로 전달하여 제공
+		/*
+		return jdbcTemplate.query(sql, new RowMapper<Student>() {
+			@Override
+			public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return Student.builder()
+						.no(rs.getInt("no"))
+						.name(rs.getString("name"))
+						.phone(rs.getString("phone"))
+						.address(rs.getString("address"))
+						.birthday(rs.getString("birthday"))
+						.build();
+			}
+		});
+		*/
+		
+		return jdbcTemplate.query(sql, new StudentRowMapper());
 	}
-
 }
+
+
+
+
+
+
