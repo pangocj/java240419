16ba835e3dd7bdc96935c2f9bca5ad533dd70721
itwill <itwill@DESKTOP-7T51VDU>
2024-08-30 12:51:24 +0900

    240830

--- spring/src/main/java/xyz/itwill09/controller/JoinController.java
+++ spring/src/main/java/xyz/itwill09/controller/JoinController.java
@@ -11,4 +11,11 @@ public class JoinController {
 	public String join() {
 		return "join_form";
 	}
+	
+	//전달값(회원정보)을 매개변수로 제공받아 Request Scope 속성값으로 저장하고 속성값을 출력하는
+	//JSP 문서의 뷰이름을 반환하는 요청 처리 메소드
+	@RequestMapping(value = "/join", method = RequestMethod.POST)
+	public String join() {
+		return "join_display";
+	}
 }
