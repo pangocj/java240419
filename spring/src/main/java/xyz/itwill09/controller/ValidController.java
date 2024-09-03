48d6c707a2025b1876d610bbb2b7fae09ca12e21
itwill <itwill@DESKTOP-7T51VDU>
2024-09-03 11:24:05 +0900

    240903

--- spring/src/main/java/xyz/itwill09/controller/ValidController.java
+++ spring/src/main/java/xyz/itwill09/controller/ValidController.java
@@ -1,13 +1,21 @@
 package xyz.itwill09.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import xyz.itwill09.dto.Employee;
 
 @Controller
 public class ValidController {
 	@RequestMapping(value = "/valid/html", method = RequestMethod.GET)
 	public String html() {
 		return "valid/html_form";		
+	}
+	
+	@RequestMapping(value = "/valid/html", method = RequestMethod.POST)
+	public String html(@ModelAttribute Employee employee) {
+		return "valid/result";		
 	}
 }
