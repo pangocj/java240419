f8db80bf3783e9ad824b3c2267c8e6f3c3a3822f
itwill <itwill@DESKTOP-7T51VDU>
2024-09-04 12:43:26 +0900

    240904

---
+++ spring/src/main/java/xyz/itwill09/controller/StudentController.java
@@ -0,0 +1,12 @@
+package xyz.itwill09.controller;
+
+import org.springframework.stereotype.Controller;
+
+//SpringMVC 기능을 사용해 웹프로그램을 작성하는 방법
+// => 테이블 >> DTO 클래스 >> DAO 클래스(MyBatis) >> Service 클래스 >> Controller 클래스
+// >> 단위 프로그램 테스트 - 테스트 프로그램(JUnit) >> JSP 문서(HTML 문서)와 결합하여 실행(통합 테스트)
+
+@Controller
+public class StudentController {
+
+}
