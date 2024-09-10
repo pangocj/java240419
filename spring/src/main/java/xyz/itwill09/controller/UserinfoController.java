e9185d06c9653e123f74fb4cae9d377e070799c7
itwill <itwill@DESKTOP-7T51VDU>
2024-09-10 13:13:32 +0900

    240910

--- spring/src/main/java/xyz/itwill09/controller/UserinfoController.java
+++ spring/src/main/java/xyz/itwill09/controller/UserinfoController.java
@@ -1,10 +1,12 @@
 package xyz.itwill09.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import lombok.RequiredArgsConstructor;
+import xyz.itwill09.dto.Userinfo;
 import xyz.itwill09.service.UserinfoService;
 
 @Controller
@@ -18,4 +20,23 @@ public class UserinfoController {
 	public String write() {
 		return "userinfo/user_write";
 	}
+	
+	//회원정보를 전달받아 USERINFO 테이블의 행으로 삽입하고 로그인 페이지를 요청할 수 있는
+	//URL 주소를 반환하는 요청 처리 메소드
+	@RequestMapping(value = "/write", method = RequestMethod.POST)
+	public String write(@ModelAttribute Userinfo userinfo) {
+		userinfoService.addUserinfo(userinfo);
+		return "redirect:/userinfo/login";
+	}
+
+	//인증정보를 입력받기 위한 JSP 문서의 뷰이름을 반환하는 요청 처리 메소드
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "userinfo/user_login";
+	}
 }
+
+
+
+
+
