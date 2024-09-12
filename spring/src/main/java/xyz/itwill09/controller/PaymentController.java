c44c4bf4605096b49cbb34d10e7c1710cb413451
itwill <itwill@DESKTOP-7T51VDU>
2024-09-12 17:27:53 +0900

    240912

---
+++ spring/src/main/java/xyz/itwill09/controller/PaymentController.java
@@ -0,0 +1,10 @@
+package xyz.itwill09.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pay")
+public class PaymentController {
+
+}
