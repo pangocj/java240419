c4df116e5b999cc9e829b9190d60711666a7bea1
itwill <itwill@DESKTOP-7T51VDU>
2024-09-19 15:08:30 +0900

    240919

---
+++ security/src/main/java/xyz/itwill/security/LoginController.java
@@ -0,0 +1,16 @@
+package xyz.itwill.security;
+
+import org.springframework.stereotype.Controller;
+
+//create table users(username varchar2(100) primary key, password varchar2(100), enabled varchar2(1));
+
+//insert into users values('abc123', '123456', '1');
+//insert into users values('opq456', '123456', '1');
+//insert into users values('xyz789', '123456', '1');
+//insert into users values('test000', '123456', '0');
+//commit;
+
+@Controller
+public class LoginController {
+
+}
