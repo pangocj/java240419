1ebf30bcae5d375b5231941666bc57422be3c78c
itwill <itwill@DESKTOP-7T51VDU>
2024-09-24 13:20:30 +0900

    240924

---
+++ security/src/main/java/xyz/itwill/auth/KakaoController.java
@@ -0,0 +1,12 @@
+package xyz.itwill.auth;
+
+//OAuth2.0 기능을 사용한 인증 처리 방법 
+// => Google, Kakao, Naver 등의 Social 로그인 기능을 사용한 인증 처리
+//1.scribejava-apis 라이브러리를 프로젝트에 빌드 처리 - 메이븐 : pom.xml
+//2.Social 로그인 기능을 제공하는 사이트에 접속하여 OpenAPI를 사용할 수 있도록 환경 설정 
+
+//카카오의 로그인 OpenAPI를 사용해 로그인 하는 방법
+//1.Kakao Developers 페이지 접속하여 애플리케이션 생성
+public class KakaoController {
+
+}
