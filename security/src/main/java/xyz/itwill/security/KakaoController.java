44eb4f0c49cd24fac48384ed5f1e33827dec3c3b
itwill <itwill@DESKTOP-7T51VDU>
2024-09-25 11:00:24 +0900

    240924

--- security/src/main/java/xyz/itwill/security/KakaoController.java
+++ security/src/main/java/xyz/itwill/security/KakaoController.java
@@ -13,18 +13,19 @@ import com.github.scribejava.core.model.OAuth2AccessToken;
 import lombok.RequiredArgsConstructor;
 import xyz.itwill.auth.KaKaoLoginBean;
 
-//OAuth2.0 기능을 사용한 인증 처리
+//Spring Security는 OAuth2.0을 사용한 인증 처리 기능 제공
 // => Google, Kakao, Naver 등의 Social 로그인 기능을 사용한 인증 처리
 
 //카카오의 로그인 OpenAPI를 사용해 간편 로그인 하는 방법
 //1.Kakao Developers 페이지 접속 - Kakao 로그인
-//2.[내 애플리케이션] >> [애플리케이션 추가하기] >> 앱 이름, 회사명, 카테고리 입력 >> 저장
-//3.생성된 애플리케이션 >> [플랫폼] >> [Web 플랫폼 등록] >> 사이트 도메인 추가 >> 저장 
-//4.생성된 애플리케이션 >> [앱키] >> [REST API 키] 복사 
+//2.[내 애플리케이션] >> [애플리케이션 추가하기] >> 앱 아이콘, 앱 이름, 회사명, 카테고리 입력 >> 저장
+// => [비즈니스] >> [개인 개발자 비즈 앱 전환] >> 2차 인증 >> 비즈 앱 전환 목적 : [이메일 필수 동의] >> 전환
+//3.생성된 애플리케이션 >> [앱키] >> [REST API 키] 복사 
+//4.생성된 애플리케이션 >> [플랫폼] >> [Web 플랫폼 등록] >> 사이트 도메인 추가 >> 저장 
 //5.생성된 애플리케이션 >> [카카오 로그인] >> [활성화 설정]의 상태를 [ON]으로 변경
 //  >> [Redirect URI 등록] >> Redirect URI 주소 입력 >> 저장
 // => Redirect URI 주소 : http://localhost:8000/security/kakao/callback
-//6.생성된 애플리케이션 >> [카카오 로그인] >> [동의 항목] >> 응답 받을 개인 정보 설정
+//6.생성된 애플리케이션 >> [카카오 로그인] >> [동의 항목] >> 동의 항목 설정 - 동의된 항목만 응답받아 사용 가능 
 //7.생성된 애플리케이션 >> [카카오 로그인] >> [보안] >> Client Secret 코드 발급 및 복사 
 // >> 활성화 상태로 변경
 //8.scribejava-apis 라이브러리와 json-simple 라이브러리를 프로젝트에 빌드 처리 - 메이븐 : pom.xml
@@ -49,6 +50,7 @@ public class KakaoController {
 			, HttpSession session) throws IOException {
 		OAuth2AccessToken accessToken=kaKaoLoginBean.getAccessToken(session, code, state);
 		String apiResult=kaKaoLoginBean.getUserProfile(accessToken);
+		//apiResult = {"id":3719292067,"connected_at":"2024-09-25T01:56:26Z","properties":{"nickname":"내삶의선물","profile_image":"http://k.kakaocdn.net/dn/dazM9Q/btsJEGW5mS6/J5HBuuTUjmBfLwAAajWbqk/img_640x640.jpg","thumbnail_image":"http://k.kakaocdn.net/dn/dazM9Q/btsJEGW5mS6/J5HBuuTUjmBfLwAAajWbqk/img_110x110.jpg"},"kakao_account":{"profile_nickname_needs_agreement":false,"profile_image_needs_agreement":false,"profile":{"nickname":"내삶의선물","thumbnail_image_url":"http://k.kakaocdn.net/dn/dazM9Q/btsJEGW5mS6/J5HBuuTUjmBfLwAAajWbqk/img_110x110.jpg","profile_image_url":"http://k.kakaocdn.net/dn/dazM9Q/btsJEGW5mS6/J5HBuuTUjmBfLwAAajWbqk/img_640x640.jpg","is_default_image":false,"is_default_nickname":false},"has_email":true,"email_needs_agreement":false,"is_email_valid":true,"is_email_verified":true,"email":"ocj1778@hanmail.net"}}
 		System.out.println("apiResult = "+apiResult);
 		
 		return "redirect:/";
