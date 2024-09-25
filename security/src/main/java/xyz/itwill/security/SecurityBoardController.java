58edc769964e3f4227bdc9cc55b90b7ca0bd3371
itwill <itwill@DESKTOP-7T51VDU>
2024-09-25 16:32:22 +0900

    240925

--- security/src/main/java/xyz/itwill/security/SecurityBoardController.java
+++ security/src/main/java/xyz/itwill/security/SecurityBoardController.java
@@ -2,7 +2,6 @@ package xyz.itwill.security;
 
 import java.util.Map;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
