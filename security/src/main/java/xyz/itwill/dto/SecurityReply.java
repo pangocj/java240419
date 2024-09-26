03c3d9b31f725d7ce49eb824e6274bf4f1eb3274
itwill <itwill@DESKTOP-7T51VDU>
2024-09-26 14:50:44 +0900

    240926

---
+++ security/src/main/java/xyz/itwill/dto/SecurityReply.java
@@ -0,0 +1,21 @@
+package xyz.itwill.dto;
+
+import lombok.Data;
+
+/*
+create table security_reply(num number primary key, writer varchar2(100), content varchar2(1000)
+	, regdate date, board_num number, constraint reply_board_num_fk foreign key(board_num) 
+	references security_board(num) on delete cascade);
+	    
+create sequence security_reply_seq;   
+*/
+
+@Data
+public class SecurityReply {
+	private int num;
+	private String writer;//작성자(아이디)
+	private String content;
+	private String regdate;
+	private int boardNum;
+	private String name;//작성자(이름)
+}
