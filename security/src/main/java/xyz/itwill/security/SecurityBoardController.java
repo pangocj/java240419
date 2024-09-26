08f7fb1e463bf35c7e8b4bedf2f8f76a940ba298
itwill <itwill@DESKTOP-7T51VDU>
2024-09-26 11:16:43 +0900

    240926

--- security/src/main/java/xyz/itwill/security/SecurityBoardController.java
+++ security/src/main/java/xyz/itwill/security/SecurityBoardController.java
@@ -2,6 +2,7 @@ package xyz.itwill.security;
 
 import java.util.Map;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -28,11 +29,18 @@ public class SecurityBoardController {
 		return "board/board_list";
 	}
 	
+	//@PreAuthorize : 요청 처리 메소드가 실행되기 전에 권한을 설정하기 위한 어노테이션
+	//value 속성 : 권한(ROLE)을 속성값으로 설정 - SpEL 사용 가능
+	// => value 속성외에 다른 속성이 없는 경우 속성값만 설정 가능
+	//@PostAuthorize : 요청 처리 메소드가 실행된 후에 권한을 설정하기 위한 어노테이션
+	//@Secured : 권한(ROLE)을 속성값으로 설정 - SpEL 사용 불가능
+	@PreAuthorize("isAuthenticated()")
 	@RequestMapping(value= "/register", method = RequestMethod.GET)
 	public String register() {
 		return "board/board_register";
 	}
 	
+	@PreAuthorize("isAuthenticated()")
 	@RequestMapping(value= "/register", method = RequestMethod.POST)
 	public String register(@ModelAttribute SecurityBoard board) {
 		board.setSubject(HtmlUtils.htmlEscape(board.getSubject()));
