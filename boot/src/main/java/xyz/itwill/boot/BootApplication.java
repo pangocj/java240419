276f4d7ff21d3c3125d49e3ba3ccb983427ee977
itwill <itwill@DESKTOP-7T51VDU>
2024-09-30 12:56:30 +0900

    240930

---
+++ boot/src/main/java/xyz/itwill/boot/BootApplication.java
@@ -0,0 +1,13 @@
+package xyz.itwill.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+
+}
---
+++ boot/src/test/java/xyz/itwill/boot/BootApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.itwill.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
