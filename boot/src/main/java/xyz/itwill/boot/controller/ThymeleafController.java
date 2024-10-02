06bd9ba8a75eb4608b9f43f9d3fa56be702fca0d
itwill <itwill@DESKTOP-7T51VDU>
2024-10-02 11:59:51 +0900

    241002

--- boot/src/main/java/xyz/itwill/boot/controller/ThymeleafController.java
+++ boot/src/main/java/xyz/itwill/boot/controller/ThymeleafController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import xyz.itwill.boot.dto.User;
+
 //Thymeleaf : 서버에서 HTML 문서 또는 XML 문서를 생성하여 제공하는 Server Side Template Engine
 // => HTML 태그를 기반으로 [th:속성명="속성값"]을 추가하여 동적으로 뷰(View)를 생성하여 제공
 // => 서버를 실행하지 않으면 정적인 HTML 문서로 사용되며 서버를 실행하면 동적인 HTML 문서로 
@@ -18,8 +20,14 @@ public class ThymeleafController {
 	public String text(Model model) {
 		model.addAttribute("dataOne", "Hi, Thymeleaf!!!");
 		model.addAttribute("dataTwo", "<font color='red'>Hi, Thymeleaf!!!</font>");
+		
+		User user=User.builder().id("abc123").name("홍길동").email("abc@itwill.xyz").build();
+		model.addAttribute("user", user);
+		
 		return "result";
 	}
+	
+	
 }
 
 
